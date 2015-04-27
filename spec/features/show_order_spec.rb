4b995ee8ad140302fd4811ccc248638b3b829f56
Nick Stewart <nick.j.stewart@gmail.com>
2015-04-27 19:05:46 +0100

    refactor spec with movie_url method

--- spec/features/show_order_spec.rb
+++ spec/features/show_order_spec.rb
@@ -2,21 +2,21 @@ describe "Viewing and individual order" do
   
   it "shows the order's details" do
     
-    order1 = Order.create(customer: "Acme1",
+    order = Order.create(customer: "Acme1",
                           model_number: "XYZ22",
                           description: "New New Thing",
                           sales_person: "Jill",
                           total: 678.00,
                           terms: "30 Days")
                           
-    visit "http://example.com/orders/1"
+    visit order_url(order)
     
-    expect(page).to have_text(order1.customer)
-    expect(page).to have_text(order1.model_number)
-    expect(page).to have_text(order1.description)
-    expect(page).to have_text(order1.sales_person)
-    expect(page).to have_text(order1.total)
-    expect(page).to have_text(order1.terms)
+    expect(page).to have_text(order.customer)
+    expect(page).to have_text(order.model_number)
+    expect(page).to have_text(order.description)
+    expect(page).to have_text(order.sales_person)
+    expect(page).to have_text(order.total)
+    expect(page).to have_text(order.terms)
   end
 end
 
