fbd7d4e4eb9b3c2363682bc009dc5ce4e943ae23
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-01 14:26:52 +0100

    add invoiced on field to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426122545) do
+ActiveRecord::Schema.define(version: 20150501131916) do
 
   create_table "orders", force: true do |t|
     t.string   "customer"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150426122545) do
     t.string   "terms"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "invoiced_on"
   end
 
 end
