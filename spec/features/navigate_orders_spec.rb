8a9df2862c5c70e8d9ca6cefdff1b6c5f2db3106
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-01 08:46:53 +0100

    add spec for nav between list and detail

--- spec/features/navigate_orders_spec.rb
+++ spec/features/navigate_orders_spec.rb
@@ -15,4 +15,20 @@ describe 'Navigting Orders' do
     expect(current_path).to eq(orders_path)
                           
   end
+  
+  it 'allows navigation from the listing page to the order detail page' do
+    order = Order.create(customer: "Acme1",
+                          model_number: "XYZ22",
+                          description: "New New Thing",
+                          sales_person: "Jill",
+                          total: 678.00,
+                          terms: "30 Days")
+  
+    visit orders_url
+    
+    click_link order.description
+    
+    expect(current_path).to eq(order_path(order))
+                          
+  end
 end
