b902a9a7290d667fd0a057a75abfe1051a6f2523
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-02 11:59:36 +0100

    add some default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Order.create!([
+  {
+    customer: "Retailer1",
+    model_number: "XYZ22",
+    description: "New New Thing",
+    sales_person: "Joe",
+    total: 678.00,
+    delivery_due_on: "2015-05-2"
+  },
+  {
+    customer: "Retailer2",
+    model_number: "ADS99",
+    description: "Another New Thing",
+    sales_person: "Jill",
+    total: 907.00,
+    delivery_due_on: "2015-05-2"
+  },
+  {
+    customer: "Retailer3",
+    model_number: "MKU23",
+    description: "And Another Thing",
+    sales_person: "Tina",
+    total: 821.00,
+    delivery_due_on: "2015-05-2"
+  },
+  {
+    customer: "Retailer4",
+    model_number: "SKU33",
+    description: "Yet Another New Thing",
+    sales_person: "Sam",
+    total: 3400.00,
+    delivery_due_on: "2015-05-2"
+  }
+])
