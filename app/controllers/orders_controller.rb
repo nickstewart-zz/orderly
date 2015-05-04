55674a3dcd2c5039bfb14dc5f8d207155009ea54
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-04 14:31:36 +0100

    add update action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,4 +10,11 @@ class OrdersController < ApplicationController
   def edit
     @order = Order.find(params[:id])
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    order_params = params.require(:order).permit!
+    @order.update(order_params)
+    redirect_to @order
+  end
 end
