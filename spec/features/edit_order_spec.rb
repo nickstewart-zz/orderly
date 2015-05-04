7697827cdd5958db79ea3142b5ae2d5b9869cbc8
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-04 11:22:36 +0100

    add edit order spec

--- spec/features/edit_order_spec.rb
+++ spec/features/edit_order_spec.rb
@@ -3,11 +3,11 @@ describe "Editing an order" do
   it "updates the order and shows the order's updated details" do
     order = Order.create(order_attributes)
     
-    visit order_url(movie)
+    visit order_url(order)
     
     click_link 'Edit'
     
-    expect(current_path).to eq(edit_order_path(movie))
+    expect(current_path).to eq(edit_order_path(order))
         
     expect(find_field('Title').value).to eq(order.title)
   end
