04f0ebc637f49eb96bf81b5599e7c6144cceb6ac
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-04 14:32:20 +0100

    add update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'orders/' => 'orders#index'
   get 'orders/:id' => 'orders#show', as: 'order'
   get 'orders/:id/edit' => 'orders#edit', as: 'edit_order'
+  patch 'orders/:id' => 'orders#update'
 end
