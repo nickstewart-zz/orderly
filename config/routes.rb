553f9149627878b503e0bf353ea961ac4164fb3c
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-04 12:11:34 +0100

    add edit_order_route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'orders#index'
   
   get 'orders/' => 'orders#index'
-  get 'orders/:id' => 'orders#show', as: "order"
+  get 'orders/:id' => 'orders#show', as: 'order'
+  get 'orders/:id/edit' => 'orders#edit', as: 'edit_order'
 end
