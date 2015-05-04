41ba73f3671ca474ebadbc554abb144ca2de7427
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-04 15:18:03 +0100

    add new_order route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'orders#index'
   
-  get 'orders/' => 'orders#index'
+  get 'orders' => 'orders#index'
+  get 'order/new' => 'orders#new', as: 'new_order'
   get 'orders/:id' => 'orders#show', as: 'order'
   get 'orders/:id/edit' => 'orders#edit', as: 'edit_order'
   patch 'orders/:id' => 'orders#update'
