37b359045da1cc9a5ca2942bd664b8c641763be1
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-06 15:17:13 +0100

    fix association name error caused by migration error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
-  belongs_to :order_id
+  belongs_to :order
+  
+  validates :code, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
