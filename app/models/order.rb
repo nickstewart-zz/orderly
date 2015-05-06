534cd06baeb335d6373448f2c7c8716d17d142be
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-06 15:15:20 +0100

    fix association name and add cascading deletes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  has_many :product
+  has_many :products, dependent: :destroy
     
   def delivery_due?
     delivery_due_on.blank? || delivery_due_on < Date.today
