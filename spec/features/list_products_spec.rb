8f9c399db47e88ebac51f868638c5640fc60abbc
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-07 12:14:33 +0100

    fix product attribute errors

--- spec/features/list_products_spec.rb
+++ spec/features/list_products_spec.rb
@@ -2,11 +2,11 @@ describe "Viewing a list of products" do
   
   it "shows the products for a specific order" do
     order1 = Order.create(order_attributes(customer: "Bluedog"))
-    product1 = order1.products.create(review_attributes(code: "O-22"))
-    product2 = order1.products.create(review_attributes(code: "F-35 IR/RW"))
+    product1 = order1.products.create(product_attributes(code: "O-22"))
+    product2 = order1.products.create(product_attributes(code: "F-35 IR/RW"))
 
     order2 = Order.create(order_attributes(customer: "TFOA"))
-    product3 = order2.products.create(review_attributes(code: "F-38"))
+    product3 = order2.products.create(product_attributes(code: "F-38"))
     
     visit order_products_url(order1)
         
