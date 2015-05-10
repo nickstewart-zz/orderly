2a6bce20a37cd971726efdb119c83a70edabbc6c
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-10 11:47:58 +0100

    Add nested product attributes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :products, dependent: :destroy
+  accepts_nested_attributes_for :products
     
   def delivery_due?
     delivery_due_on.blank? || delivery_due_on < Date.today
