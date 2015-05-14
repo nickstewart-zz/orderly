493c0a428189b9cd47b3e378ea6b9cd20e049a7f
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-14 17:54:46 +0100

    add product create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @order = Order.find(params[:order_id])
     @product = @order.products.new 
   end
+
+  def create
+    @order = Order.find(params[:order_id])
+    @product = @order.products.new(params.require(:product).permit!)
+    if @product.save
+      redirect_to @order, notice: "Product successfully added to order!"
+    else
+      render :new
+    end
+  end
 end
