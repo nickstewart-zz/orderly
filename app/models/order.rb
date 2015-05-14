3927e9e37bb563a6a0bb6feafaac8d9e99109d92
Nick Stewart <nick.j.stewart@gmail.com>
2015-05-14 18:01:01 +0100

    add order total method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,15 @@
 class Order < ActiveRecord::Base
   has_many :products, dependent: :destroy
   accepts_nested_attributes_for :products
+
+  validates :customer_company, presence: true
+  validates :customer_name, presence: true 
     
   def delivery_due?
     delivery_due_on.blank? || delivery_due_on < Date.today
+  end
+
+  def total
+    products.sum(:price)
   end
 end
