7332cf8f4b59c585946a5fdb7111f5069b10548a
Nick Stewart <nick.j.stewart@gmail.com>
2015-06-17 12:15:22 +0100

    athenticate session

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'user sign in' do
+describe 'Signing in' do
 	it 'prompts for email and password' do
 		visit root_url
 
@@ -10,5 +10,34 @@ describe 'user sign in' do
 
 		expect(page).to have_field(:email)
 		expect(page).to have_field(:password)
-    end
+	end
+
+	it 'signs in user with a valid email/password combination' do
+	  user = User.create(user_attributes)
+
+	  visit root_url
+
+	  click_link 'Sign In'
+
+	  fill_in 'Email', with: user.email
+	  fill_in 'Password', with: user.password
+	  
+	  click_button 'Sign In'
+	  expect(current_path).to eq(user_path(user))
+	  expect(page).to have_text("Welcome back, #{user.name}!")
+	end
+
+	it 'does not sign in an invalid user email/password combination' do
+	  user = User.create(user_attributes)
+	  visit root_url
+
+	  click_link 'Sign In'
+
+	  fill_in 'Email', with: user.email
+	  fill_in 'Password', with: "noentry"
+
+	  click_button 'Sign In'
+
+	  expect(page).to have_text("Invalid email or password.")
+	end
 end
