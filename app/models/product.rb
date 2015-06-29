966c7da55993a3b22b7735a22dd77d015840518f
Nick Stewart <nick.j.stewart@gmail.com>
2015-06-29 16:13:13 +0100

    declare one user has many products model relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :order
-  
+  belongs_to :product
+
   validates :code, presence: true
   validates :options_standard, presence: true
   validates :price, presence: true
