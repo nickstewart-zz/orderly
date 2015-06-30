ab9d9813725343ed614733925202829cfaeb74ae
Nick Stewart <nick.j.stewart@gmail.com>
2015-06-30 16:12:42 +0100

    assign current_user to product.user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :require_signin
+
   def index
   	@order = Order.find(params[:order_id])
     @products = @order.products
@@ -6,12 +8,13 @@ class ProductsController < ApplicationController
 
   def new
     @order = Order.find(params[:order_id])
-    @product = @order.products.new 
+    @product = @order.products.new
   end
 
   def create
     @order = Order.find(params[:order_id])
     @product = @order.products.new(params.require(:product).permit!)
+    @product.user = current_user
     if @product.save
       redirect_to @order, notice: "Product successfully added to order!"
     else
